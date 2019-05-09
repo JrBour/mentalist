d3367fd462b0aafe5468dc97bdc44bebdf9fcaca
JrBour <calaruni@hotmail.com>
2019-05-09 21:44:12 +0200

    :construction: Add column email_hashed in the table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->boolean('admin');
             $table->string('email')->unique();
+            $table->string('email_hashed');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();

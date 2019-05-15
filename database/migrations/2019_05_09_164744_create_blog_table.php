9164b24fb582763e64ce3a18e3754e7d2dfef4de
JrBour <calaruni@hotmail.com>
2019-05-15 23:12:26 +0200

    :construction: Add user id in the table comment

--- database/migrations/2019_05_09_164744_create_blog_table.php
+++ database/migrations/2019_05_09_164744_create_blog_table.php
@@ -26,7 +26,7 @@ class CreateBlogTable extends Migration
             $table->bigInteger('author_id')->unsigned()->nullable();
             $table->foreign('author_id')->references('id')->on('users')->onDelete('set null');
             $table->unsignedInteger('category_id')->nullable();
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
 
@@ -34,6 +34,8 @@ class CreateBlogTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->longText('content');
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
             $table->unsignedInteger('article_id');
             $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
             $table->timestamps();

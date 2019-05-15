e75e510029d0bc4bb0e641f7958a54ec6d942b1d
JrBour <calaruni@hotmail.com>
2019-05-15 22:47:24 +0200

    :sparkles: Create the CRUD route for articles (#5)

---
+++ app/Article.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $fillable = ['title', 'content', 'author_id', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category', 'category_id');
+    }
+
+    public function author()
+    {
+        return $this->belongsTo('App\User', 'author_id');
+    }
+}
